dbbb5679f3ab1eda0a81b42e7f7c2ad3343dec50
cyonits <cyonitss@gmail.com>
2023-08-09 17:58:29 +0900

    스프링 프로젝트 생성 (#7)

---
+++ BE/stack-overflow/src/main/java/com/seb45_022/preproject/stackoverflow/StackOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.seb45_022.preproject.stackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverflowApplication.class, args);
+	}
+
+}

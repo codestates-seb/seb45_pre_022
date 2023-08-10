307b1c5e185ed152891d0b7e38f77e37ae204480
Dohee Kim <kdhwh09@gmail.com>
2023-08-11 03:01:17 +0900

    [FE] Feat: 푸터 컴포넌트 (#16)

--- FE/client/src/components/Footer.js
+++ FE/client/src/components/Footer.js
@@ -1,121 +1,184 @@
 import styled from 'styled-components';
 
-import footerObj from './footerObj';
-
-const FooterContainer = styled.footer`
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-`;
-
-const FooterWrapper = styled.div`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: 0;
-  width: 100%;
-  height: 300px;
-  padding: 12px 32px;
+const StyledFooter = styled.footer`
   background-color: #202225;
   color: #babfc4;
+`;
+
+const FooterContainer = styled.div`
   display: flex;
-  justify-content: space-around;
   flex-wrap: wrap;
-  flex-direction: row;
+  justify-content: space-between;
+  align-items: space-between;
+  max-width: 1264px;
+  margin: 0 auto;
+  padding: 24px 16px;
+  @media (max-width: 990px) {
+    flex-direction: column;
+    align-items: stretch;
+  }
 `;
 
-const Section = styled.div`
-  margin-bottom: 10px;
-  color: #babfc4;
-  font-size: 13px;
+const Logo = styled.img`
+  width: 70px;
+  height: 50px;
+  padding-right: 20px;
+  @media (max-width: 500px) {
+    display: none;
+  }
 `;
 
-const LinkRow = styled.div`
-  min-width: 200px;
+const Nav = styled.nav`
   display: flex;
-  align-items: center;
-  padding: 5px 0px;
-  margin-top: 2px;
+  flex-direction: row;
+  gap: 20px;
+  flex-grow: 1;
+`;
 
-  > h6 {
-    display: flex;
-    align-items: center;
+const Column = styled.div`
+  flex: 1;
+  h5 {
     font-size: 13px;
-    font-weight: 600;
-    margin: 0;
-    padding: 0;
+    font-weight: 700;
     color: #9199a1;
+    margin: 0;
+    margin-bottom: 12px;
+    &:hover {
+      color: #ffffff;
+    }
   }
 
-  > h6:hover {
-    color: #ffffff;
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    margin-bottom: 4px;
+  }
+
+  a {
+    color: #8a8d90;
+    text-decoration: none;
+    &:hover {
+      color: #ffffff;
+    }
   }
 `;
 
 const AdditionalInfo = styled.div`
   display: flex;
-  align-items: center;
-  padding-left: 20px;
-  align-items: flex-start;
-  font-size: 10px;
-  font-weight: 600;
-  color: #9199a1;
-  > h6 {
+  flex-direction: column;
+  padding: 20px;
+  justify-content: space-between;
+  flex: 1;
+  ul {
+    list-style: none;
     display: flex;
-    align-items: center;
-    margin-right: 10px;
+    gap: 20px;
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    font-size: 12px;
+    font-weight: 600;
+    a {
+      color: #8a8d90;
+      text-decoration: none;
+      &:hover {
+        color: #d1d2d3;
+      }
+    }
+  }
+
+  p {
+    font-size: 12px;
+    color: #8a8d90;
+    margin-top: 16px;
   }
 `;
-
-const Paragraph = styled.p`
-  margin-top: 200px;
-  padding: 10px;
-`;
-
 const Footer = () => {
+  const footerColumns = [
+    { title: 'STACK OVERFLOW', links: ['Questions', 'Help'] },
+    {
+      title: 'PRODUCTS',
+      links: ['Teams', 'Advertising', 'Collectives', 'Talent'],
+    },
+    {
+      title: 'COMPANY',
+      links: [
+        'About',
+        'Press',
+        'Work Here',
+        'Legal',
+        'Privacy Policy',
+        'Terms of Service',
+        'Contact Us',
+      ],
+    },
+    {
+      title: 'STACK EXCHANGE NETWORK',
+      links: [
+        'Technology',
+        'Culture & recreation',
+        'Life & arts',
+        'Science',
+        'Professional',
+        'Business',
+        'API',
+        'Data',
+      ],
+    },
+  ];
+
   return (
-    <FooterContainer>
-      <FooterWrapper>
-        {footerObj.map((el, i) => (
-          <Section key={i}>
-            {i === 0 ? (
-              <>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Stack_Overflow_icon.svg/1024px-Stack_Overflow_icon.svg.png?20190716190036"
-                  alt="Logo"
-                  width="50"
-                />
-                <h3>{el.title}</h3>
-              </>
-            ) : (
-              <>
-                <h3>{el.title}</h3>
-                {i === 5 ? (
-                  <AdditionalInfo>
-                    {el.links.map((link, j) => (
-                      <h6 key={j}>{link.name}</h6>
-                    ))}
-                  </AdditionalInfo>
-                ) : (
-                  el.links.map((link, j) => (
-                    <LinkRow key={j}>
-                      <h6>{link.name}</h6>
-                    </LinkRow>
-                  ))
-                )}
-                {i === 5 && (
-                  <Paragraph>
-                    Site design / logo © 2023 Stack Exchange Inc; user
-                    contributions licensed under CC BY-SA. rev 2023.8.9.43572
-                  </Paragraph>
-                )}
-              </>
-            )}
-          </Section>
-        ))}
-      </FooterWrapper>
-    </FooterContainer>
+    <StyledFooter>
+      <FooterContainer>
+        <Logo
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Stack_Overflow_icon.svg/1024px-Stack_Overflow_icon.svg.png?20190716190036"
+          alt="logo"
+        />
+        <Nav>
+          {footerColumns.map((column) => (
+            <Column key={column.title}>
+              <h5>{column.title}</h5>
+              <ul>
+                {column.links.map((link) => (
+                  <li key={link}>
+                    <a href="/">{link}</a>
+                  </li>
+                ))}
+              </ul>
+            </Column>
+          ))}
+        </Nav>
+        <AdditionalInfo>
+          <ul>
+            <li>
+              <a href="/">Blog</a>
+            </li>
+            <li>
+              <a href="/">Facebook</a>
+            </li>
+            <li>
+              <a href="/">Twitter</a>
+            </li>
+            <li>
+              <a href="/">LinkedIn</a>
+            </li>
+            <li>
+              <a href="/">Instagram</a>
+            </li>
+          </ul>
+          <p>
+            Site design / logo © 2023 Stack Exchange Inc; user contributions
+            licensed under CC BY-SA. rev 2023.8.9.43572
+          </p>
+        </AdditionalInfo>
+      </FooterContainer>
+    </StyledFooter>
   );
 };
 

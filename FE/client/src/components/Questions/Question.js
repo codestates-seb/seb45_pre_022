b2b2a6ccd6803318a7b5f124a45bbfbf0fced39a
Saekyul <saekyulyang@gmail.com>
2023-08-10 13:25:45 +0900

    [FE] Feat: 질문 내용 요약에서 글이 길어지면 뒤에 ... 표시 (#17)

--- FE/client/src/components/Questions/Question.js
+++ FE/client/src/components/Questions/Question.js
@@ -25,6 +25,7 @@ const Info = styled.div`
 const QuestionSummary = styled.div`
   display: flex;
   flex-direction: column;
+  width: 611px;
 `;
 
 const QuestionTitle = styled.div`
@@ -34,7 +35,16 @@ const QuestionTitle = styled.div`
   font-weight: 600;
 `;
 
-const QuestionContent = styled.div``;
+const EllipsisText = styled.div`
+  display: -webkit-box;
+  overflow: hidden;
+  max-height: ${(props) => props.maxLine * props.lineHeight}px;
+  -webkit-line-clamp: ${(props) => props.maxLine};
+  -webkit-box-orient: vertical;
+  line-height: ${(props) => props.lineHeight}px;
+`;
+
+const QuestionContent = styled(EllipsisText)``;
 
 const Question = () => {
   return (
@@ -46,8 +56,11 @@ const Question = () => {
       </InfoContainer>
       <QuestionSummary>
         <QuestionTitle>리액트 컴포넌트에서 문제가 생겼습니다!</QuestionTitle>
-        <QuestionContent>
-          컴포넌트를 분할하고 폴더를 옮겼는데 갑자기 오류가 발생했습니다.
+        <QuestionContent maxLine={2} lineHeight={20}>
+          컴포넌트를 분할하고 폴더를 옮겼는데 갑자기 오류가 발생했습니다. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Iure, repellat
+          quis, totam nam inventore est possimus ea sunt non velit quo animi
+          error autem iusto deleniti vitae perferendis corrupti quibusdam!
         </QuestionContent>
       </QuestionSummary>
     </QuestionContainer>

eec21809e8c0c0df24cd489672b3cccae22eee48
Dohee Kim <kdhwh09@gmail.com>
2023-08-10 16:28:06 +0900

    [FE] Feat: Footer 컴포넌트 작성(#16)

--- FE/client/src/App.js
+++ FE/client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Questions from './components/Questions';
 import Question from './components/Question';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" element={<Questions />} />
         <Route path="/questions/:id" element={<Question />} />
       </Routes>
-      <footer style={{ backgroundColor: 'gray' }}>footer</footer>
+      <Footer />
     </BrowserRouter>
   );
 }

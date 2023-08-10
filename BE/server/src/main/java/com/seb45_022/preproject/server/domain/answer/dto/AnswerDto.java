9d23df239701faa2a3f90626cc88b00f8268e61a
45hyewon <haewon0611@gmail.com>
2023-08-10 17:35:30 +0900

    답변 CRUD기능 기초 구현입니다 (#20)

--- BE/server/src/main/java/com/seb45_022/preproject/server/domain/answer/dto/AnswerDto.java
+++ BE/server/src/main/java/com/seb45_022/preproject/server/domain/answer/dto/AnswerDto.java
@@ -1,12 +1,10 @@
 package com.seb45_022.preproject.server.domain.answer.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
 
 public class AnswerDto {
     @Getter

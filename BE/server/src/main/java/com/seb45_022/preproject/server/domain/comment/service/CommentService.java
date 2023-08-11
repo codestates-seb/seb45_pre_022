1027e290a91646ff5835a0cf0d8df1b506007fee
45hyewon <haewon0611@gmail.com>
2023-08-11 13:45:39 +0900

    [BE] Feat: AnswerService 클래스 수정, CommentService 추가 (#20)

--- BE/server/src/main/java/com/seb45_022/preproject/server/domain/comment/service/CommentService.java
+++ BE/server/src/main/java/com/seb45_022/preproject/server/domain/comment/service/CommentService.java
@@ -1,7 +1,12 @@
 package com.seb45_022.preproject.server.domain.comment.service;
 
+import com.seb45_022.preproject.server.domain.answer.service.AnswerService;
 import com.seb45_022.preproject.server.domain.comment.entity.Comment;
 import com.seb45_022.preproject.server.domain.comment.repository.CommentRepository;
+import com.seb45_022.preproject.server.domain.member.repository.MemberRepository;
+import com.seb45_022.preproject.server.domain.member.service.MemberService;
+import com.seb45_022.preproject.server.global.exception.businessLogic.BusinessLogicException;
+import com.seb45_022.preproject.server.global.exception.code.ExceptionCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,11 +18,19 @@ import java.util.Optional;
 public class CommentService {
 
     private final CommentRepository commentRepository;
-    public CommentService(CommentRepository commentRepository) {
+    private final AnswerService answerService;
+    private final MemberService memberService;
+    public CommentService(CommentRepository commentRepository,
+                          AnswerService answerService,
+                          MemberService memberService) {
         this.commentRepository = commentRepository;
+        this.answerService = answerService;
+        this.memberService = memberService;
     }
 
     public Comment createComment(Comment comment) {
+        answerService.findVerifiedAnswer(comment.getAnswer().getAnswerId());
+        memberService.findVerifiedMember(comment.getMember().getMemberId());
         comment.setCreatedAt(LocalDateTime.now());
         comment.setLastModifiedAt(LocalDateTime.now());
 
@@ -25,17 +38,20 @@ public class CommentService {
     }
 
     public Comment updateComment(Comment comment) {
+        answerService.findVerifiedAnswer(comment.getAnswer().getAnswerId());
+        memberService.findVerifiedMember(comment.getMember().getMemberId());
+
         Comment foundComment = findVerifiedComment(comment.getCommentId());
         foundComment.setLastModifiedAt(LocalDateTime.now());
         return commentRepository.save(foundComment);
     }
 
-    private Comment findVerifiedComment(long commentId) {
+    public Comment findVerifiedComment(long commentId) {
         Optional<Comment> optionalComment =
                 commentRepository.findById(commentId);
         Comment findComment =
                 optionalComment.orElseThrow(() ->
-                        new RuntimeException());
+                        new BusinessLogicException(ExceptionCode.COMMENT_NOT_FOUND));
         return findComment;
     }
     public void deleteComment(long commentId) {

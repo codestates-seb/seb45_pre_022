6e7adca98ede12e11ee76b88ce4799f060f25f4f
Saekyul <saekyulyang@gmail.com>
2023-08-14 10:21:33 +0900

    [FE] Feat: Tags 디자인 (#42)

--- FE/client/src/Pages/AskPage/Contents.js
+++ FE/client/src/Pages/AskPage/Contents.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Content,
   SubTitle,
@@ -6,6 +7,7 @@ import {
   NextButton,
   TextArea,
 } from './AskPageStyles';
+import { styled } from 'styled-components';
 
 export const TitleContent = ({
   currentStep,
@@ -65,7 +67,61 @@ export const ProblemContent = ({
   );
 };
 
-export const TagsContent = ({ currentStep, onHandleTags }) => {
+// 인풋창 안에서 태그를 관리할 스타일을 정의합니다.
+const InputWrapper = styled.div`
+  display: inline-flex;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 35px;
+  background-color: white;
+`;
+
+// 개별 태그 스타일을 정의합니다.
+const Tag = styled.div`
+  display: inline-flex;
+  background-color: #e1ecf4;
+  color: #39739d;
+  padding: 5px;
+  margin: 5px;
+  border-radius: 5px;
+  font-size: 12px;
+  cursor: pointer;
+`;
+
+// 실제 입력을 처리하는 Input 스타일을 정의합니다.
+const StyledInput = styled.input`
+  border: none;
+  outline: none;
+  flex-grow: 1;
+`;
+
+// TagsContent 컴포넌트를 만들기 위한 함수입니다.
+export const TagsContent = ({ currentStep }) => {
+  // 사용자가 입력한 태그를 관리하기 위한 state 입니다.
+  const [tags, setTags] = useState([]);
+
+  // 사용자가 입력한 태그를 처리하는 함수입니다.
+  const handleTagInput = (e) => {
+    // 사용자가 스페이스바를 누를 경우
+    if (e.key === ' ') {
+      // 스페이스바 앞에 입력된 텍스트를 가져옵니다.
+      const newTag = e.target.value.trim();
+
+      // 텍스트가 비어있지 않다면 tags 배열에 추가합니다.
+      if (newTag !== '') {
+        setTags([...tags, newTag]);
+        e.target.value = ''; // 입력 창을 비웁니다.
+      }
+    }
+  };
+
+  // 태그를 삭제하는 함수입니다.
+  const handleTagDelete = (index) => {
+    // 해당 인덱스의 태그를 제외하고 새 배열을 생성합니다.
+    const newTags = tags.filter((_, i) => i !== index);
+    setTags(newTags);
+  };
+
   return (
     <Content disabled={currentStep < 3}>
       <SubTitle>tags</SubTitle>
@@ -73,11 +129,18 @@ export const TagsContent = ({ currentStep, onHandleTags }) => {
         Add up to 5 tags to describe what your question is about. Start typing
         to see suggestions.
       </Description>
-      <Input
-        type="text"
-        placeholder="e.g. (javascript) (react)"
-        onChange={onHandleTags}
-      />
+      <InputWrapper>
+        {tags.map((tag, index) => (
+          <Tag key={index} onClick={() => handleTagDelete(index)}>
+            {tag}
+          </Tag>
+        ))}
+        <StyledInput
+          type="text"
+          placeholder="e.g. (javascript) (react)"
+          onKeyDown={handleTagInput} // 키보드의 키를 누를 때마다 handleTagInput 함수를 호출합니다.
+        />
+      </InputWrapper>
     </Content>
   );
 };

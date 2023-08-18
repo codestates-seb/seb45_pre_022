96d0e1f68bd3c7aa51f831c673cfaddd8e5cb246
Saekyul <saekyulyang@gmail.com>
2023-08-18 14:26:04 +0900

    [FE] Feat: 페이지네이션 디자인 #119

--- FE/client/src/components/Questions/Questions.js
+++ FE/client/src/components/Questions/Questions.js
@@ -5,17 +5,51 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import queryString from 'query-string';
-import Loading from '../../Loading';
 
 const MainContainer = styled.div`
   padding: 24px;
+`;
+
+const Button = styled.button`
+  &:hover {
+    background-color: gray;
+  }
+
+  &active {
+    background-color: #f48225;
+  }
+`;
+
+const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 16px;
+`;
+
+const PaginationButton = styled.button`
+  margin: 0 4px;
+  padding: 4px 8px;
+  border: none;
+  background-color: ${({ active }) => (active ? '#f48225' : 'white')};
+  color: ${({ active }) => (active ? 'white' : 'black')};
+  cursor: ${({ active }) => (active ? 'default' : 'pointer')};
+
+  &:hover {
+    background-color: ${({ active }) => (active ? '#f48225' : 'gray')};
+    color: white;
+  }
+
+  &:disabled {
+    cursor: default;
+    opacity: 0.5;
+  }
 `;
 
 const Questions = () => {
   const [questions, setQuestions] = useState([]);
   const [totalElements, setTotalElements] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const itemsPerPage = 10;
 
   const location = useLocation();
   const { search } = queryString.parse(location.search);
@@ -51,19 +85,44 @@ const Questions = () => {
     fetchQuestions();
   }, [search, currentPage, itemsPerPage]);
 
+  const totalPages = Math.ceil(totalElements / itemsPerPage);
+  const pageNumbers = [];
+
+  for (
+    let i = Math.max(1, currentPage - 4);
+    i <= Math.min(totalPages, currentPage + 4);
+    i++
+  ) {
+    pageNumbers.push(i);
+  }
+
   return (
     <MainContainer>
-      <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-        이전
-      </button>
-      <button
-        onClick={handleNextPage}
-        disabled={currentPage * itemsPerPage >= totalElements}
-      >
-        다음
-      </button>
       <QuestionsTopBar totalElements={totalElements} />
       <Question questions={questions} />
+      <PaginationContainer>
+        <PaginationButton
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1}
+        >
+          이전
+        </PaginationButton>
+        {pageNumbers.map((pageNumber) => (
+          <PaginationButton
+            key={pageNumber}
+            onClick={() => setCurrentPage(pageNumber)}
+            active={pageNumber === currentPage}
+          >
+            {pageNumber}
+          </PaginationButton>
+        ))}
+        <PaginationButton
+          onClick={handleNextPage}
+          disabled={currentPage * itemsPerPage >= totalElements}
+        >
+          다음
+        </PaginationButton>
+      </PaginationContainer>
     </MainContainer>
   );
 };

c8632ef0c4aa86166defd4250dfb5ae9955fbf01
Saekyul <saekyulyang@gmail.com>
2023-08-21 10:52:53 +0900

    [FE] Feat: 에딧 컨테이너 생성 #79

--- FE/client/src/Pages/Users/UsersPage.js
+++ FE/client/src/Pages/Users/UsersPage.js
@@ -3,13 +3,15 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Loading from '../../Loading';
 import { getCookieValue } from '../../custom/getCookie';
+import { StyledButton } from '../../components/Buttons/AskButton';
 
 const Button = styled.button`
   border: 1px solid lightgrey;
-  width: 80px;
   height: 40px;
   background-color: white;
   border-radius: 7px;
+  padding: 7px;
+  cursor: pointer;
 `;
 
 const Container = styled.div`
@@ -31,18 +33,33 @@ const TestImage = styled.div`
 const UserName = styled.div`
   font-size: 2rem;
   font-weight: 600;
-  margin: 4px 4px 12px 4px;
+  margin-bottom: 10px;
 `;
 
 const UserInfo = styled.div`
   display: flex;
   flex-direction: column;
+  margin-left: 15px;
+  justify-content: center;
+`;
+
+const EditContainer = styled.div`
+  margin: 20px;
 `;
 
 const MyPage = () => {
   const [user, setUser] = useState(null);
+  const [isEdit, setIsEdit] = useState(false);
 
   const accessToken = getCookieValue('access_token');
+
+  const onHandleEdit = () => {
+    setIsEdit(true);
+  };
+
+  const onCloseEdit = () => {
+    setIsEdit(false);
+  };
 
   useEffect(() => {
     axios
@@ -80,8 +97,31 @@ const MyPage = () => {
             <div>Total Answers: {user.totalAnswers}</div>
           </UserInfo>
         </div>
-        <Button>Edit Profile</Button>
+        {!isEdit ? (
+          <Button onClick={onHandleEdit}>Edit Profile</Button>
+        ) : (
+          <Button onClick={onCloseEdit}>Close</Button>
+        )}
       </Summary>
+      {isEdit ? (
+        <EditContainer>
+          <div>
+            <h3>Display Name</h3>
+            <input
+              type="text"
+              value={user.displayName}
+              style={{
+                width: '300px',
+                padding: '10px',
+                borderRadius: '5px',
+                border: '1px solid gray',
+                margin: '10px 0',
+              }}
+            />
+          </div>
+          <StyledButton>Save Profile</StyledButton>
+        </EditContainer>
+      ) : null}
     </Container>
   );
 };
